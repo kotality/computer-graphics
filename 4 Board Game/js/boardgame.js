1c67d85d278a5f5c70a29d1b9891da3b38091b21
k.castro <k.castro14@gmail.com>
2017-10-22 16:43:12 -0400

    added checker game pieces red and black

--- 4 Board Game/js/boardgame.js
+++ 4 Board Game/js/boardgame.js
@@ -1,13 +1,21 @@
 var renderer;
 var scene;
 var camera;
-var geometry;
-var circleGeometry, circleGeometry2;
-var lineHorizontalGeometry, lineVerticalGeometry;
+var red, black;
 
-// How you can move on the board
-var col = [];
-var row = [];
+// var baseBoard = [
+// 	[0, 1, 0, 1, 0, 1, 0, 1],
+// 	[1, 0, 1, 0, 1, 0, 1, 0],
+// 	[0, 1, 0, 1, 0, 1, 0, 1],
+// 	[0, 0, 0, 0, 0, 0, 0, 0],
+// 	[0, 0, 0, 0, 0, 0, 0, 0],
+// 	[2, 0, 2, 0, 2, 0, 2, 0],
+// 	[0, 2, 0, 2, 0, 2, 0, 2],
+// 	[2, 0, 2, 0, 2, 0, 2, 0]
+// ];
+
+// var tiles = [];
+// var pieces = [];
 
 Physijs.scripts.worker = 'libs/physijs_worker.js';
 Physijs.scripts.ammo = 'ammo.js';
@@ -29,8 +37,10 @@ function init()
 	// 	mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 	// });
 
-	board();	
-	playerPieces();
+	board();
+	populateBoard();	
+	// playerPiecesRed();
+	// playerPiecesBlack();
 
 	// Call render
 	render();
@@ -38,6 +48,7 @@ function init()
 
 function render()
 {	
+
 	if (Key.isDown(Key.Z))
 	{
 		camera.rotation.x += 0.03;
@@ -100,9 +111,9 @@ function addSpotLight()
 function setupCamera()
 {
 	camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-	camera.position.z = 80;
+	camera.position.z = 60;
 	camera.position.x = 0;
-	camera.position.y = 0; 
+	// camera.position.y = -70; 
 	// camera.lookAt(scene.position)
 	// camera.lookAt(new THREE.Vector3(0,0,0));
 }
@@ -117,16 +128,82 @@ function setupRenderer()
 
 function board() 
 {
-	var planeGeometry = new THREE.BoxGeometry(99, 99, 5); // width, height, depth
+	var planeGeometry = new THREE.BoxGeometry(80, 80, 5); // width, height, depth
 	// var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xf9ebd1 });
 	var planeMaterial = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('images/checkersBoard.gif') });
 	var ground = new THREE.Mesh(planeGeometry, planeMaterial);
 	scene.add(ground);	
 }
 
-function playerPieces() 
+function populateBoard() 
 {
+	/*--------------- RED --------------*/
+	// Bottom row
+	playerPiecesRed(-35, -35);	
+	playerPiecesRed(-15, -35);
+	playerPiecesRed(5, -35);
+	playerPiecesRed(25, -35);
+	// Middle row
+	playerPiecesRed(-25, -25);
+	playerPiecesRed(-5, -25);
+	playerPiecesRed(15, -25);
+	playerPiecesRed(35, -25);
+	// Top row
+	playerPiecesRed(-35, -15);
+	playerPiecesRed(-15, -15);
+	playerPiecesRed(5, -15);
+	playerPiecesRed(25, -15);
+	
+	/*--------------- BLACK --------------*/
+	// Top row
+	playerPiecesBlack(-25, 35);
+	playerPiecesBlack(-5, 35);
+	playerPiecesBlack(15, 35);
+	playerPiecesBlack(35, 35);
+	// Middle row
+	playerPiecesBlack(-35, 25);
+	playerPiecesBlack(-15, 25);
+	playerPiecesBlack(5, 25);
+	playerPiecesBlack(25, 25);
+	// Bottom row
+	playerPiecesBlack(-25, 15);
+	playerPiecesBlack(-5, 15);
+	playerPiecesBlack(15, 15);
+	playerPiecesBlack(35, 15);
+	
+}
+function playerPiecesRed(X, Y) 
+{
+	var texture = THREE.ImageUtils.loadTexture('images/redTile.jpg');
+
+	var geometry = new THREE.CylinderGeometry(4.5, 4.5, 2.5, 32); // radiusTop, radiusBottom, height, radiusSegments
+	var material = new Physijs.createMaterial(new THREE.MeshBasicMaterial({ map: texture }), .95, .95);
+	red = new THREE.Mesh(geometry, material);
+
+	red.position.x = X;
+	red.position.y = Y;
+	red.position.z = 4;
+
+	red.rotation.x = 30; 
+	scene.add(red);
+
 	// objectLoader.onLoadComplete = function (obj) { scene.add(obj) } 
+}
+
+function playerPiecesBlack(X, Y) 
+{
+	var texture = THREE.ImageUtils.loadTexture('images/blackTile.jpg');
+
+	var geometry = new THREE.CylinderGeometry(4.5, 4.5, 2.5, 32); // radiusTop, radiusBottom, height, radiusSegments
+	var material = new Physijs.createMaterial(new THREE.MeshBasicMaterial({ map: texture }), .95, .95);
+	black = new THREE.Mesh(geometry, material);
+
+	black.position.x = X;
+	black.position.y = Y;
+	black.position.z = 4;
+
+	black.rotation.x = 30;
+	scene.add(black);
 }
 
 init();
