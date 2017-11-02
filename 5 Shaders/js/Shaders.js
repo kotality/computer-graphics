d66360457ae13116a283967d71b7cec720198493
k.castro <k.castro14@gmail.com>
2017-11-02 05:04:59 -0400

    Added one custom shader to 4 objects

--- 5 Shaders/js/Shaders.js
+++ 5 Shaders/js/Shaders.js
@@ -1,8 +1,10 @@
-var camera;
-var scene;
-var renderer;
-var spotlight; //--------------- Don't have yet
-var cube, cubeSimple, cubeSimpleMotion, sphere;
+var camera, scene, renderer, spotlight; 
+var mouse = {x:0, y:0};
+var cube, cubeSimple, cone, sphere;
+
+document.onmousemove = getMouseXY;
+
+init();
 
 function init()
 {
@@ -10,10 +12,12 @@ function init()
 
     camera();      
     renderer();
+    spotlight();
 
+    // Objects
     cube();
+    coneMotion();
     cubeSimple();
-    cubeSimpleMotion();
     sphere();
     
     animate();   
@@ -32,84 +36,117 @@ function renderer()
     document.body.appendChild(renderer.domElement);
 }
 
-// Creates a loop that causes renderer to draw scene 60 times per second.
+function spotlight()
+{
+    spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(10,20,20); // x, y, z
+
+    spotLight.castShadow = true;
+
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+
+    spotLight.shadow.camera.near = 500; 
+    spotLight.shadow.camera.far = 4000;
+    spotLight.shadow.camera.fov = 30;
+
+    scene.add(spotLight);
+}
+
+function getMouseXY(e) 
+{
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+}
+
 function animate()
 { 
-    /* 
-     - requestAnimationFrame(): Tells the browser that you wish to perform an 
-       animation and requests that the browser call a specified function to 
-       update an animation before the next repaint.
-     - (animate): A parameter specifying a function to call when it's time to 
-       update your animation for the next repaint. 
-    */
     requestAnimationFrame(animate); //------------------------------------------
 
-    /*
-     - This will be run every frame (60 times per second), and give the cube a 
-       nice rotation animation. Basically, anything you want to move or change 
-       while the app is running has to go through the animate loop. 
-     - You can of course call other functions from there, so that you don't end 
-       up with a animate function that's hundreds of lines.
-    */
     Rotation();
 
     renderer.render(scene, camera); //------------------------------------------
 }
 
+function Rotation() 
+{
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+
+    cubeSimple.rotation.x += 0.01;
+    cubeSimple.rotation.y += 0.01;
+    cubeSimple.rotation.z += 0.01;
+
+    // cone.rotation.x += 0.01;
+    cone.rotation.y += 0.01;
+    // cone.rotation.z += 0.01;
+
+    sphere.rotation.x += 0.01;
+    // sphere.rotation.y += 0.01;
+    // sphere.rotation.z += 0.01;
+}
+
+// Simple shader with pattern
 function cube() 
 {
-    var geometry = new THREE.BoxGeometry(1, 1, 1); // width, height, depth
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var geometry = new THREE.BoxBufferGeometry(1, 1, 1); // width, height, depth
+    var material = new THREE.ShaderMaterial(
+    {
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+    });
     cube = new THREE.Mesh(geometry, material);
+
     cube.position.x = -4;
     cube.position.y = 2;
     scene.add(cube);
 }
 
+// Simple shader with pattern that has motion
+function coneMotion() 
+{
+    var geometry = new THREE.ConeGeometry(0.8, 1.5);
+    var material = new THREE.ShaderMaterial(
+        {
+            vertexShader: document.getElementById('vertexShader').textContent,
+            fragmentShader: document.getElementById('fragmentShader').textContent
+        });
+    cone = new THREE.Mesh(geometry, material);
+
+    cone.position.x = 4;
+    cone.position.y = -2;
+    scene.add(cone);
+}
+
+// Shader for cube
 function cubeSimple()
 {
-    var geometry = new THREE.BoxGeometry(1, 1, 1); // width, height, depth
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.ShaderMaterial(
+    {
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+    });
     cubeSimple = new THREE.Mesh(geometry, material);
+
     cubeSimple.position.x = -4;
     cubeSimple.position.y = -2;
     scene.add(cubeSimple);
 }
 
-function cubeSimpleMotion()
-{
-    var geometry = new THREE.BoxGeometry(1, 1, 1); // width, height, depth
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    cubeSimpleMotion = new THREE.Mesh(geometry, material);
-    cubeSimpleMotion.position.x = 4;
-    cubeSimpleMotion.position.y = -2;
-    scene.add(cubeSimpleMotion);
-}
-
+// Shader for sphere
 function sphere() 
 {
-    var geometry = new THREE.SphereGeometry(1);//, 32, 32); // radius, widthSegments, heightSegments,
-    var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    var geometry = new THREE.SphereGeometry(1);
+    var material = new THREE.ShaderMaterial(
+    {
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+    });
     sphere = new THREE.Mesh(geometry, material);
+
     sphere.position.x = 4;
     sphere.position.y = 2;
     scene.add(sphere);
 }
-
-function Rotation()
-{
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-    cubeSimple.rotation.x += 0.1;
-    cubeSimple.rotation.y += 0.1;
-
-    cubeSimpleMotion.rotation.x += 0.1;
-    cubeSimpleMotion.rotation.y += 0.1;
-
-    sphere.rotation.x += 0.1;
-    sphere.rotation.y += 0.1;
-}
-
-// window.onload = init;
-init();
